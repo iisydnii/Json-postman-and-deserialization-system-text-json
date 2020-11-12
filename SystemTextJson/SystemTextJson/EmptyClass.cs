ab43fd3181e65b1c46f3e64d2654c7f3483ad2f9
iisydnii <61031342+iisydnii@users.noreply.github.com>
2020-11-12 12:14:07 -0500

    update

---
+++ SystemTextJson/SystemTextJson/EmptyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SystemTextJson
+{
+    public class EmptyClass
+    {
+       
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Heelppppp");
+            }
+      
+    }
+}
---
+++ SystemTextJson/SystemTextJson/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SystemTextJson
+{
+    class Program
+    {
+        
+    }
+}
